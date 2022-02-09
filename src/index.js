0c93f007ac02702be17525ef2eb7cf6d89707b53
David TRAN <david.tran.devweb@gmail.com>
2022-02-09 20:07:07 +0100

    Add public_url

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './styles/index.scss';
 import App from './components/App/App';
 
+console.log(process.env.REACT_APP_PUBLIC_URL);
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
